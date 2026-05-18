30cf1ac1454ad12b4ff72297df976fa6f296696c
ShrastiJain <shrastijain1305@gmail.com>
2026-05-18 14:15:16 +0530

    10 commit

--- Task1-Frontend/src/pages/AuthRoute.jsx
+++ Task1-Frontend/src/pages/AuthRoute.jsx
@@ -6,7 +6,13 @@ const AuthRoute = () => {
   let {loading}=useSelector((state)=>state.loading)
   let {user}=useSelector((state)=>state.user)
   
-  if(loading)return <h1>Loading...</h1>
+  if (loading) {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <div className="w-14 h-14 border-4 border-gray-700 border-t-white rounded-full animate-spin"></div>
+    </div>
+  );
+}
 
   if(user){
     return <Navigate to='/home'/>
--- Task1-Frontend/src/pages/ProtectedRoute.jsx
+++ Task1-Frontend/src/pages/ProtectedRoute.jsx
@@ -5,7 +5,13 @@ import { Navigate, Outlet } from 'react-router'
 const ProtectedRoute = () => {
    let {loading}=useSelector((state)=>state.loading)
     let {user}=useSelector((state)=>state.user)
-    if(loading)return <h1>Loading...</h1>
+  if (loading) {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <div className="w-14 h-14 border-4 border-gray-700 border-t-white rounded-full animate-spin"></div>
+    </div>
+  );
+}
     
   if(!user){
     return <Navigate to='/'/>
