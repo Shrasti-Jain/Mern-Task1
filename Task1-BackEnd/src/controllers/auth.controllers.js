b9a943f484bd79f3a73bfc637fadbd611e68f59b
ShrastiJain <shrastijain1305@gmail.com>
2026-05-18 14:28:28 +0530

    11 commit

--- Task1-BackEnd/src/controllers/auth.controllers.js
+++ Task1-BackEnd/src/controllers/auth.controllers.js
@@ -121,7 +121,7 @@ let forgotPasswordController=asyncHandler(async(req,res)=>{
 
     let rawToken=user.generateToken()
 
-    let rawLink=`http://localhost:3000/api/auth/user/reset-password/${rawToken}`
+    let rawLink=`https://mern-task1-4sct.onrender.com/api/auth/user/reset-password/${rawToken}`
      
    await sentMailTo(
   email,
